a46a9f55a82ce50120a57e15f19929058c3432dd
dan2812 <danylo.jordan.2005@gmail.com>
2019-11-20 20:35:53 +0200

    hw1js

---
+++ jsclass/index.js
@@ -0,0 +1,86 @@
+"use strict";
+
+// console.log("HEllo world");
+
+// let number = 6;
+// let string ="String";
+// let boolean = true; //false
+
+// let a = 2;
+// let b = 3;
+
+// let result = 2 + 3;
+
+// console.log("Our resul",result);
+
+// let apple;
+
+// console.log(apple);
+
+// console.log(a);
+// var a = 3;
+ 
+// // var  c = confirm();
+// // console.log(c);
+
+
+// const z = prompt("Enter your name");
+// console.log(z);
+
+//  const a = 9;
+ 
+//  const b = 9;
+
+//  let resul = a % b;
+
+//  console.log(result);
+
+//  console.log(a === Number (b));
+
+// let a = "25px";
+// console.log(Number.parseInt(a) + 50 + "px");
+
+
+// console.log(Number.parseFloat(a) + 50);
+// let a ="h44adf";
+// console.log(Number.isNaN(+ a));
+// console.log( 0.1 + 0.3 === 0.4)
+
+// let a = 0.1 + 0.2;
+// console.log( 0.1 + (0.2).toFixed(1));
+
+// console.log((Math.random() * 10 + 1).toFixed(0));
+// const a = 1,
+// b = 5,
+// c = 8;
+// console.log(Math.max(a, b, c));
+
+// console.log(Math.ceil(0.5));
+// console.log(Math.floor(1,5));
+// console.log(Math.round(1.6));
+// console.log(Math.pow(3, 2));
+
+// const age = 25
+
+// console.log("Hello I'm "+ age + "yeaars old");
+
+// const name = "Rodion";
+
+// console.log(name.length);
+// console.log(name.indexOf("d"));
+// console.log(name.toLowerCase)
+// console.log(name.toLowerCase)
+// console.log(name.incLudes(name.i("m")));
+
+// const firstName = "Rodion";
+// const surName = "Romanov";
+// const age = 25;
+// console.log(
+// `Hello my name is ${firstName} ${surName} and I'm ${age} years old`); 
+
+constm name = "generator zashitnogo polia"
+let price = 1000;
+price = 2000;
+
+
+
---
+++ jsclass/j1k/index.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// const c = 10;
+
+// let result = !c < 10 || b > 10;
+
+// console.log(result);
+
+
+// const a = 10
+// if(a === 10){
+//     alert("Helllo World!!!");
+// }else if (a < 10) {
+//     confirm("Are you ok?");
+// } else {
+//     prompt ("Enter your name")
+// }
+
+
+// let num = prompt (" enter any nuber from 1-10");
+
+// if (+num > 5) {
+//     alert ("You win!!!");
+// }
+// else {
+//     alert ("Sorry you lose");
+// }
+
+// const num = 20;
+
+// let result = num === 10? console.log("10")
+// : num === 20
+// ? console.log("20")
+// : num === 30 
+// ? console.log("30")
+// :console.log(" You misss")
+
+// const correctPassword = "gregerree";
+
+// let a = prompt("введите пароль доступа");
+
+// if(a === correctPassword){
+//     alert("Доступ в секретний бункер разрешен!");
+// } else if (!a){
+//     alert("Била нажата отмена!");
+// } else{
+//     alert("неверний пароль,активирована система защити!");
+// }(
+
+const adminLogin =  'admin';
+const adminPassword = 'nbnbnbggg';
+
+let a = prompt("login please");
+if(a === adminLogin){
+    let b = prompt("password,please");
+    if(b === adminPassword){
+        alert("");
+    } else if (!b) {}
+}
+
+
+
+if(a === adminLogin){
+    alert("")
+}
+
